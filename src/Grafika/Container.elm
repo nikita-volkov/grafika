1bd3a29a2b207cbfd7645498738f2b4095970b10
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-06-25 12:46:10 +0300

    Refactor Container in terms of Attributes

--- src/Grafika/Container.elm
+++ src/Grafika/Container.elm
@@ -2,24 +2,13 @@ module Grafika.Container exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Grafika.Attributes as Attributes
 
 
 type alias Container msg = List (Html msg) -> Html msg
 
 horizontal : Int -> Int -> Container msg
-horizontal width height =
-  div
-    [
-      style "display" "inline-block",
-      style "width" (String.fromInt width ++ "px"),
-      style "height" (String.fromInt height ++ "px")
-    ]
+horizontal width height = div (Attributes.horizontalBlock ++ Attributes.size width height)
 
 vertical : Int -> Int -> Container msg
-vertical width height =
-  div
-    [
-      style "display" "block",
-      style "width" (String.fromInt width ++ "px"),
-      style "height" (String.fromInt height ++ "px")
-    ]
+vertical width height = div (Attributes.verticalBlock ++ Attributes.size width height)
