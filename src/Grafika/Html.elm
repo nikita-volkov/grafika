72993b2e9104ba8be95e7584d654d4c93901f92f
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-07-15 08:34:26 +0300

    Add a non-abstract Html helpers module

---
+++ src/Grafika/Html.elm
@@ -0,0 +1,91 @@
+module Grafika.Html exposing (..)
+
+import Html exposing (..)
+import Html.Attributes as Attribute
+import Grafika.CoreExtensions.List as List
+import Grafika.String as String
+
+
+empty : Html msg
+empty = text ""
+
+row : Int -> List (Html msg) -> Html msg
+row spacing children =
+  let
+    wrapHead html =
+      div
+        [
+          Attribute.style "display" "inline-block"
+        ]
+        [
+          html
+        ]
+    wrapTail html =
+      div
+        [
+          Attribute.style "display" "inline-block",
+          Attribute.style "margin-left" (String.intPx spacing)
+        ]
+        [
+          html
+        ]
+    in
+      List.mapHeadAndTail wrapHead wrapTail children |>
+      Html.div
+        [
+          Attribute.style "text-align" "left"
+        ]
+
+column : Int -> List (Html msg) -> Html msg
+column spacing children =
+  let
+    wrapTail html =
+      div
+        [
+          Attribute.style "margin-top" (String.intPx spacing)
+        ]
+        [
+          html
+        ]
+    in
+      List.mapTail wrapTail children |>
+      Html.div []
+
+centerHorizontally : Html msg -> Html msg
+centerHorizontally html =
+  div
+    [
+      Attribute.style "text-align" "center"
+    ]
+    [
+      div
+        [
+          Attribute.style "display" "inline-block"
+        ]
+        [html]
+    ]
+
+centerVertically : Html msg -> Html msg
+centerVertically html =
+  div
+    [
+      Attribute.style "width" "100%",
+      Attribute.style "height" "100%"
+    ]
+    [
+      div
+        [
+          Attribute.style "height" "100%",
+          Attribute.style "width" "100%",
+          Attribute.style "display" "table"
+        ]
+        [
+          div
+            [
+              Attribute.style "display" "table-cell",
+              Attribute.style "vertical-align" "middle",
+              Attribute.style "width" "100%"
+            ]
+            [html]
+        ]
+    ]
