97c5748c5a758830fa4aa870b5e4f905bd7bc7be
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-08-05 20:16:43 +0300

    Ditch the Attribute namespace

--- src/Grafika/Html.elm
+++ src/Grafika/Html.elm
@@ -4,7 +4,7 @@ Wraps the typical patterns in HTML.
 -}
 
 import Html exposing (..)
-import Html.Attributes as Attribute
+import Html.Attributes exposing (..)
 import Grafika.CoreExtensions.List as List
 import Grafika.String as String
 import Markdown
@@ -19,8 +19,8 @@ row spacing children =
     wrapHead html =
       div
         [
-          Attribute.style "height" "100%",
-          Attribute.style "display" "inline-block"
+          style "height" "100%",
+          style "display" "inline-block"
         ]
         [
           html
@@ -28,9 +28,9 @@ row spacing children =
     wrapTail html =
       div
         [
-          Attribute.style "height" "100%",
-          Attribute.style "display" "inline-block",
-          Attribute.style "margin-left" (String.intPx spacing)
+          style "height" "100%",
+          style "display" "inline-block",
+          style "margin-left" (String.intPx spacing)
         ]
         [
           html
@@ -39,7 +39,7 @@ row spacing children =
       List.mapHeadAndTail wrapHead wrapTail children |>
       Html.div
         [
-          Attribute.style "text-align" "left"
+          style "text-align" "left"
         ]
 
 column : Int -> List (Html msg) -> Html msg
@@ -48,7 +48,7 @@ column spacing children =
     wrapTail html =
       div
         [
-          Attribute.style "margin-top" (String.intPx spacing)
+          style "margin-top" (String.intPx spacing)
         ]
         [
           html
@@ -61,12 +61,12 @@ centerHorizontally : Html msg -> Html msg
 centerHorizontally html =
   div
     [
-      Attribute.style "text-align" "center"
+      style "text-align" "center"
     ]
     [
       div
         [
-          Attribute.style "display" "inline-block"
+          style "display" "inline-block"
         ]
         [html]
     ]
@@ -75,22 +75,22 @@ centerVertically : Html msg -> Html msg
 centerVertically html =
   div
     [
-      Attribute.style "width" "100%",
-      Attribute.style "height" "100%"
+      style "width" "100%",
+      style "height" "100%"
     ]
     [
       div
         [
-          Attribute.style "height" "100%",
-          Attribute.style "width" "100%",
-          Attribute.style "display" "table"
+          style "height" "100%",
+          style "width" "100%",
+          style "display" "table"
         ]
         [
           div
             [
-              Attribute.style "display" "table-cell",
-              Attribute.style "vertical-align" "middle",
-              Attribute.style "width" "100%"
+              style "display" "table-cell",
+              style "vertical-align" "middle",
+              style "width" "100%"
             ]
             [html]
         ]
@@ -103,8 +103,8 @@ stylesheet : String -> Html msg
 stylesheet css =
   node "style"
     [
-      Attribute.rel "stylesheet",
-      Attribute.type_ "text/css"
+      rel "stylesheet",
+      type_ "text/css"
     ]
     [
       text css
